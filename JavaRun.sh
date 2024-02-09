e33226eff5903f2a4deb5f4eb915a8f1bbfccec7
Mansur <mustafin.mansur02@gmail.com>
2024-02-09 01:51:39 +0000

    added remove instruction in Java version

--- JavaRun.sh
+++ JavaRun.sh
@@ -3,8 +3,12 @@ BIN_DIR="bin"
 MAIN_CLASS="MatrixProduct"
 OUTPUT="output/results_java.csv"
 
+# Clean and compile
+rm -f $BIN_DIR/*.class
+
 javac -d $BIN_DIR $SRC_DIR/*.java
 
+# Tests
 for dim in `seq 600 400 3000`; do
     java -cp $BIN_DIR $MAIN_CLASS 1 $dim $OUTPUT 
 done
